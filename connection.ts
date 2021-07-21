276b547e313924efca2dec3b1f567bedaf95a1fe
Matt <powley.matthew@gmail.com>
2021-07-21 15:00:31 -0600

    worked on connection class

--- connection.ts
+++ connection.ts
@@ -1,4 +1,5 @@
 import {Message, MessageType} from "./messages";
+import {MessageCallback} from "./messagingClient";
 
 export type SendProtocolMessage = (message: Message) => void
 
@@ -7,21 +8,43 @@ export class Connection {
     id: string;
     private readonly sendProtocolMessage: SendProtocolMessage;
     private accepted: boolean;
+    paused: boolean;
+    //messagingClient: MessagingClient;
+
+    messageListeners: Set<MessageCallback>;
 
 
     constructor(id: string, protocolMessage: SendProtocolMessage, accepted: boolean) {
       this.id = id;
       this.sendProtocolMessage = protocolMessage;
       this.accepted = accepted;
+      this.paused = false;
 
+      this.messageListeners = new Set();
     }
 
-    sendMessage<T>(body: T) {
-        this.sendProtocolMessage( {
+    async sendMessage<T>(body: T) {
+        if(this.accepted == false) {
+            //protocol error.
+        }
+
+        await this.sendProtocolMessage( {
             type: MessageType.APPLICATION_APP,
             client: this.id,
             body,
         })
+    }
+
+    addMessageListener(callback: MessageCallback) {
+        this.messageListeners.add(callback);
+    }
+
+    removeMessageListener(callback: MessageCallback) {
+        this.messageListeners.delete(callback);
+    }
+
+    clearMessageListener() {
+        this.messageListeners.clear();
     }
 
 }

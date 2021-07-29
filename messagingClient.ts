ca58d4611b058ba8486bf662262ad2a3ef78bc70
chris-luangrath <thechrisluangrath@gmail.com>
2021-07-29 16:40:00 -0600

    reorganized functions to match python library

--- messagingClient.ts
+++ messagingClient.ts
@@ -1,5 +1,7 @@
 import {Connection, _Connection} from "./connection";
+import { LockStateError, ConnectionNotFoundError } from "./errors";
 import {BaseMessage, Message, MessageType} from "./messages";
+import { Request } from "./requests";
 import { MessageOrRequest, ConnectionCallback, ConnectionCloseCallback, MessageCallback } from "./types";
 
 export class MessagingClient {
@@ -14,7 +16,7 @@ export class MessagingClient {
     connections: Map<string, Connection>;
     connectionListeners: ConnectionCallback[];
     messageListeners: MessageCallback[];
-    lock: boolean | number;
+    lock: boolean | number; // protocol
 
 
    // lock: ?protocol.Lock?;
@@ -40,82 +42,86 @@ export class MessagingClient {
     //
     // }
 
-    private async sendProtocolMessage(message: BaseMessage) {
-      this.checkOutgoingProtocolMessage(message);
-    }
+    // private aysnc receiveHandler(){
+    //     async for message in self._socket:
+    //     # TODO: Add support for binary messages
+    //     if not isinstance(message, str):
+    //         print("not string?")
+    //         continue
 
-    private notifyMessageListeners(body: unknown) {
-        this.messageListeners.forEach((callback) => callback(body))
-    }
+    //     try:
+    //         await self._on_message(protocol.deserialize(json.loads(message)))
+    //     except Exception as e:
+    //         print(e)
+    // print("it's over")
+    //   this.socket.
+    // }
 
-    private onOpen() {
+    // private async sendHandler(){
+    //   while (true) {
+    //     message = await this.
+    //   }
+    // }
 
-    }
+    // We don't need checkOutgoingProtocolMessage() or sendProtocolMessage()
+    // because we aren't using message queue
 
     private onMessage(data: string) {
-        const message = JSON.parse(data);
+      const message = JSON.parse(data);
 
-        if (message.type == MessageType.HeartbeatClient) {
-          if(!message.up){
-            message.clients.forEach(id => this.removeConnection(id));
-            return;
-          }
-          // TODO: This test might be expensive and unnecessary, consider simplifying
-          //  or removing it
-          // await this._compare_heartbeat_up_connections(message.clients)
+      if (message.type == MessageType.HeartbeatClient) {
+        if(!message.up){
+          message.clients.forEach(id => this.removeConnection(id));
+          return;
+        }
+        // TODO: This test might be expensive and unnecessary, consider simplifying
+        //  or removing it
+        // await this._compare_heartbeat_up_connections(message.clients)
+        return;
+      }
+
+      if (message.type == MessageType.Connect) {
+        if(!(message.client in this.connections)){
           return;
         }
 
-        if (message.type == MessageType.Connect) {
-          if(!(message.client in this.connections)){
-            return;
-          }
+        this.addConnection(message.client);
+        return;
+      }
 
-          this.addConnection(message.client);
+      if (message.hasAttribute("client")){
+        if(!(message.client in this.connections)) {
+          throw "AccessError, ";
+          // f"Unauthorized client '{message.client}' attempted to send an authenticated message"
+          
+        }
+      }
+
+      if (message.type == MessageType.ApplicationClient) {
+        if(message.hasAttribute("req")){ // i don't know how to write this more efficient
+          this.notifyMessageListeners((message.body, message.req));
+          this.connections[message.client].notifyMessageListeners((message.body, message.req));
           return;
+        } else {
+          this.notifyMessageListeners((message.body));
+          this.connections[message.client].notifyMessageListeners((message.body));
         }
 
-        if (message.type == MessageType.ApplicationClient) {
-            this.notifyMessageListeners(message.body);
-            this.connections[message.client].notifyMessageListeners(message.body);
-            return;
-        }
 
-        if (message.type == MessageType.Lifecycle) {
-          this.connections[message.client].paused = message.paused;
-          return
-        }
+        this.notifyMessageListeners(message.body);
+        this.connections[message.client].notifyMessageListeners(message.body);
+        return;
+      }
+
+      if (message.type == MessageType.Lifecycle) {
+        this.connections[message.client].paused = message.paused;
+        return
+      }
 
       //   raise protocol.UnhandledMessageTypeError(
       //     f"Couldn't handle message type '{message.type}'"
       //   )
 
-
-    }
-
-    private onError() {
-
-    }
-
-    private onClose() {
-
-    }
-
-    private checkOutgoingProtocolMessage(message: BaseMessage){
-      if (message.type == MessageType.Access && !(this.lock)) {
-        // raise LockStateError(
-        //   "A lock is required to send access messages to clients"
-        // )
-      }
-      if (!(message.type in [
-        MessageType.Access,
-        MessageType.ApplicationApp,
-        MessageType.DisplaySettings,
-      ])){
-        // raise protocol.UnhandledMessageTypeError(
-        //   f"Couldn't send message type '{message.type}'"
-        // )
-      } 
     }
 
     private compareHeartbeatUpConnections(connections: Connection[]){
@@ -153,50 +159,93 @@ export class MessagingClient {
     }
 
     private sendMessage<T>(body: T, requestId: string) {
-        this.connections.forEach((connection) => connection.sendMessage(body, requestId));
+      this.connections.forEach((connection) => connection.sendMessage(body, requestId));
+  }
+
+  //
+  // Client connection handling
+  // (these methods just handle updating internal state and notifying listeners
+  // _after_ connections are added/removed)
+  // 
+
+
+  private addConnection(id: string){
+    var connection = new _Connection(
+      id,
+      this.url, 
+      this.sendProtocolMessage, // Is this correct?
+      !(this.lock));
+    this.connections[id] = connection;
+    this.notifyMessageListeners(connection);
+  }
+
+  private removeConnection(id: string){
+    if(!(id in this.connections )){
+      return;
     }
+    this.connections[id].notifyCloseListeners();
+    delete this.connections[id];
+  }
 
+  // 
+  // Message listener handling
+  // 
 
-    // lock state, lock defines the max number of connections
-    //
+  addMessageListener(callback: MessageCallback) {
+    this.messageListeners.push(callback);
+  }
 
+  removeMessageListener(callback: MessageCallback) {
+    this.messageListeners.unshift(callback);
+  }
 
-    private addConnection(id: string){
-      var connection = new _Connection(
-        id,
-        this.url, 
-        this.sendProtocolMessage, // Is this correct?
-        !(this.lock));
-      this.connections[id] = connection;
-      this.notifyMessageListeners(connection);
+  private clearMessageListeners() {
+      this.messageListeners.length = 0;
+  }
+
+  private notifyMessageListeners(body: unknown) {
+      this.messageListeners.forEach((callback) => callback(body))
+  }
+
+  // 
+  // Connection listneer handling
+  // 
+
+  addConnectionListener(callback: ConnectionCallback){
+    this.connectionListeners.push(callback);
+  }
+
+  removeConnectionListener(callback: ConnectionCallback){
+    const index = this.connectionListeners.indexOf(callback);
+    if (index > -1) {
+      this.connectionListeners.splice(index, 1);
     }
+  }
 
-    private removeConnection(id: string){
-      if(!(id in this.connections )){
-        return;
-      }
-      this.connections[id].notifyCloseListeners();
-      delete this.connections[id];
-    }
+  private clearConnectionListeners(){
+    this.connectionListeners.length = 0;
+  }
 
-    //Listeners
-    addMessageListener(callback: MessageCallback) {
-      this.messageListeners.push(callback);
-    }
+  private notifyConnectionListeners(connection: _Connection){
+    this.connectionListeners.forEach(callback => {
+      callback(new Connection(connection));
+    })
+  }
 
-    removeMessageListener(callback: MessageCallback) {
-      this.messageListeners.unshift(callback);
-    }
 
-    private clearMessageListeners() {
-        this.messageListeners.length = 0;
-    }
+  private onOpen() {
 
-    private notifyConnectionListeners(connection: _Connection){
-      this.connectionListeners.forEach(callback => {
-        callback(new Connection(connection));
-      })
-    }
+  }
+
+  private onError() {
+
+  }
+
+  private onClose() {
+
+  }
+
+    
 
 
 

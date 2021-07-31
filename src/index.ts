458c6ef45e05a76e1a1c5c4ae76322bee3aa093c
chris-luangrath <thechrisluangrath@gmail.com>
2021-07-30 20:42:10 -0600

    import from messagingClient

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import { MessagingClient } from './messagingClient'
+
 export { Connection } from './connection'
-export  *  from './messages'
+
+export class Messaging extends MessagingClient{
+    constructor(url?: string){
+        if(url == null){
+            url = new URLSearchParams(location.href).get("ftMsgUrl") ?? "ws://localhost:8089/out";
+        }
+        super(url);
+    }
+}
